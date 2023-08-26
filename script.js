c2867b4997c58afcdd9c579f7c71a2e9a3a62804
sora <sorazora1.4@gmail.com>
2023-08-25 22:08:36 -0700

    rolls the dice and changes the dice image

--- script.js
+++ script.js
@@ -4,8 +4,21 @@
 const score0 = document.querySelector("#score--0");
 const score1 = document.getElementById("score--1");
 const diceImg = document.querySelector(".dice");
+const btnNewGame = document.querySelector(".btn--new");
+const btnRollDice = document.querySelector(".btn--roll");
+const btnHoldScore = document.querySelector(".btn--hold");
 
 // Initial starting screen state
 score0.textContent = 0;
 score1.textContent = 0;
 diceImg.classList.add("hidden");
+
+// Roll the dice
+btnRollDice.addEventListener("click", () => {
+  // Generate a random dice number (1 - 6)
+  const diceValue = Math.trunc(Math.random() * 6) + 1;
+
+  // Display the dice on the screen
+  diceImg.classList.remove("hidden");
+  diceImg.src = `dice-${diceValue}.png`;
+});
