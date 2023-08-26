9c167d1ff318c478e60255a0f229621122d26eb8
sora <sorazora1.4@gmail.com>
2023-08-25 21:59:20 -0700

    set the initial page state

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-'use strict';
+"use strict";
+
+// DOM Elements
+const score0 = document.querySelector("#score--0");
+const score1 = document.getElementById("score--1");
+const diceImg = document.querySelector(".dice");
+
+// Initial starting screen state
+score0.textContent = 0;
+score1.textContent = 0;
+diceImg.classList.add("hidden");
